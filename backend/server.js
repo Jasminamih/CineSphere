81d3b3f5cf6af1f2be1ad9e637ce31243f33ab52
Jasmina Mihajloska <mihajloska.jasmina@outlook.com>
2025-10-31 17:00:06 +0100

    fix: support ES modules and serve React build correctly

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
